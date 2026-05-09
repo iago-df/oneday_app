274e3451d75d62a227bad85c0e002bf953f063fd
Iago Donsión Fernández <iagodgfa@gmail.com>
2026-05-09 20:49:38 +0200

    url

--- od_backend/rest_api/urls.py
+++ od_backend/rest_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('day-entries/<int:id>/', views.DayEntriesItemView.as_view(), name='day_entries_item'),
     path('day-entries/<int:id>/close/', views.DayEntriesCloseView.as_view(), name='day_entries_close'),
     path('day-entries/<int:id>/draft-close/', views.DayEntriesDraftCloseView.as_view(), name='day_entries_draft_close'),
+    path('activities/', views.ActivitiesListView.as_view(), name='activities_list'),
 ]
